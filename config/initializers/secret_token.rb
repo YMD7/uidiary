e183e5bdb531b618001c3da7ce21239a9d23bffe
Kyo Yamada <hysteric.madam@gmail.com>
2015-03-28 22:37:10 +0900

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Uidiary::Application.config.secret_key_base = '7e3414478a84dcc66c414b87d97d47d358b6ebdce17a280c150aab5deacafc307d2ba5b663e10576e939dffbfa1e5a119c151417b05bf60cc7db95af1eba90fc'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Uidiary::Application.config.session_store :cookie_store, key: '_uidiary_session'
