2e421c3589324f40af88351476a64b9947676ed3
Kyo Yamada <hysteric.madam@gmail.com>
2015-08-17 01:50:33 +0900

    ビデオアップローダーのstore_dirをpublic/videosに変更、ついでにcache_dirもtmp/cache/に変更

--- app/uploaders/video_uploader.rb
+++ app/uploaders/video_uploader.rb
@@ -16,11 +16,11 @@ class VideoUploader < CarrierWave::Uploader::Base
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
     # "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-    "#{Rails.root}/app/assets/videos/#{@product_id}/#{@version}"
+    "#{Rails.root}/public/videos/#{@product_id}/#{@version}"
   end
 
   def cache_dir
-    "#{Rails.root}/public/tmp/"
+    "#{Rails.root}/tmp/cache/"
   end
 
   #
